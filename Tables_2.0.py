5fdd6885943a92e73c4055e4eb3b407c5bb54d32
kartheekvarma <kartheekvarma5@gmail.com>
2022-11-10 13:53:45 +0530

    Done Changes

--- Tables_2.0.py
+++ Tables_2.0.py
@@ -1,32 +1,24 @@
+
 def ForwardMultiply ( NumberValue , StartRangeValue, EndRangeValue ):
     for LoopValue in range( StartRangeValue , (EndRangeValue + 1 )):
-        print (" {} X {} = {}".format(NumberValue , LoopValue, ( Number * LoopValue )))
+        print ("\t\t{} X {} = {}".format(NumberValue , LoopValue, ( Number * LoopValue )))
 
 def ReverseMultiply ( NumberValue , StartRangeValue, EndRangeValue ):
     while ( StartRangeValue >= EndRangeValue ):
-        print (" {} X {} = {}".format(NumberValue , StartRangeValue, ( Number * StartRangeValue )))
+        print ("\t\t{} X {} = {}".format(NumberValue , StartRangeValue, ( Number * StartRangeValue )))
         StartRangeValue = StartRangeValue - 1
 
 try:
-    Number = (int(input("\n Please input the no for which you want to see the tables: ")))
-    StartRange = (int(input(" Please input the Starting Range from which you want to start: ")))
-    EndRange = (int(input(" Please input the Ending Range from which you want to End: ")))
-    
-    print ("\n")
+    Number = (int(input("\n Please input the No. for which you want to see the tables\t: ")))
+    StartRange = (int(input(" Please input the Starting Range from which you want to start\t: ")))
+    EndRange = (int(input(" Please input the Ending Range from which you want to End\t: ")))
     if ( StartRange <= EndRange ):
         ForwardMultiply( Number, StartRange, EndRange )
+        print ("\n")
     elif ( StartRange > EndRange ):
-        print ("\tPrinting the values in reverse fashion\n")
+        print ("\n\tPrinting the values in reverse Order\n")
         ReverseMultiply( Number, StartRange, EndRange )
-
-        # RevInput = str(input("\tYou have given start range lesser than end range, Please make sure you give StartRange as Lower than EndRange..\n\tDo you want to continue and show the tables in reverse order?[Yes/No]: "))    
-        # if (( RevInput.casefold() == "Yes" or RevInput.casefold() == "Y" )):
-        #     RevInput = "yes"
-        #     print (RevInput)
-        #     ForwardMultiply( Number, StartRange, EndRange )
-        # else:
-        #     print ("Hello")
-
+        print ("\n")
     else:
         print ("\tSomething went wrong. Please check the same..")
 
