a2970665689bd1f2ba20dc6674455fa1e326436c
kartheekvarma <kartheekvarma5@gmail.com>
2022-12-06 15:37:23 +0530

    Added Util Folder with Python Screenshot utility example

---
+++ Util/PythonScreenshots.py
@@ -0,0 +1,23 @@
+#import urllib.request
+#resource = urllib.request.urlopen("https://stackoverflow.com/questions/8286352/how-to-save-an-image-locally-using-python-whose-url-address-i-already-know")
+#output = open("file01.jpg","wb")
+#output.write(resource.read())
+#output.close()
+
+
+#Below Code Take Screenshot of the screen of that particular screen.
+import numpy as np
+import cv2
+import pyautogui
+
+# take screenshot using pyautogui
+image = pyautogui.screenshot()
+
+# since the pyautogui takes as a 
+# PIL(pillow) and in RGB we need to 
+# convert it to numpy array and BGR 
+# so we can write it to the disk
+image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+# writing it to the disk using opencv
+cv2.imwrite("image1.jpg", image)
